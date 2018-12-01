45cc88afb5e4fad5802e806555f1d75479b47bc8
kalzit <1196868+kalzit@users.noreply.github.com>
2018-12-01 16:07:40 +0900

    add external connection.

--- src/Application.kt
+++ src/Application.kt
@@ -3,22 +3,27 @@ package io.api.sample
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.application.log
+import io.ktor.client.HttpClient
+import io.ktor.client.call.call
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.response.readText
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
+import io.ktor.http.ContentType
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
 import io.ktor.locations.Locations
 import io.ktor.locations.get
-import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.routing
+import kotlinx.coroutines.async
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-data class Message(val content: String)
-
 @KtorExperimentalLocationsAPI
-@Location("/{name}")
-data class Index(val name: String)
+@Location("/")
+data class Index(val url: String)
 
 @KtorExperimentalLocationsAPI
 fun Application.module() {
@@ -27,8 +32,24 @@ fun Application.module() {
         gson()
     }
     routing {
-        get<Index>{ p ->
-            call.respond(Message("Hello, ${p.name}"))
+        get<Index> { p ->
+            val start = System.currentTimeMillis();
+            log.info("start")
+
+            // async {...} 内が非同期処理
+            val content = async {
+                HttpClient(Apache).use { client ->
+                    log.info("client request start: time ${System.currentTimeMillis() - start}ms")
+                    val content = client.call(p.url).response.readText()
+                    log.info("client request end: time ${System.currentTimeMillis() - start}ms")
+                    content
+                }
+            }
+
+            log.info("client processed: time ${System.currentTimeMillis() - start}ms")
+
+            // await で非同期処理の完了を待ち、結果を取得できる
+            call.respondText(content.await(), ContentType.Text.Plain)
         }
     }
 }
