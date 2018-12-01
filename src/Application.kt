5ee20a3533e53f6ad1a2af315c8a3d3131d2227c
kalzit <1196868+kalzit@users.noreply.github.com>
2018-12-01 15:32:28 +0900

    add hello world.

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,18 @@
 package io.api.sample
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+    }
 }
-
