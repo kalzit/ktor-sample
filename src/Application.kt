33721660840291823d259f1c151f735c38d98d04
kalzit <1196868+kalzit@users.noreply.github.com>
2018-12-01 15:47:01 +0900

    json format output.

--- src/Application.kt
+++ src/Application.kt
@@ -2,17 +2,24 @@ package io.api.sample
 
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+data class Message(val content: String)
+
 fun Application.module() {
+    install(ContentNegotiation) {
+        gson()
+    }
     routing {
         get("/") {
-            call.respondText("Hello World!", ContentType.Text.Plain)
+            call.respond(Message("Hello World!"))
         }
     }
 }
