2c98c74550a93625be62aa517e616aba0dfe9419
kalzit <1196868+kalzit@users.noreply.github.com>
2018-12-01 16:14:07 +0900

    create api.

--- src/Application.kt
+++ src/Application.kt
@@ -1,29 +1,32 @@
 package io.api.sample
 
+import com.google.gson.Gson
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.application.log
 import io.ktor.client.HttpClient
 import io.ktor.client.call.call
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.response.readText
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
-import io.ktor.http.ContentType
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
 import io.ktor.locations.Locations
 import io.ktor.locations.get
-import io.ktor.response.respondText
+import io.ktor.response.respond
 import io.ktor.routing.routing
 import kotlinx.coroutines.async
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @KtorExperimentalLocationsAPI
-@Location("/")
-data class Index(val url: String)
+@Location("/{page}/{lang?}")
+data class Index(val page: String, val lang: String = "")
+
+data class WikiPedia(val parse: Parse?)
+data class Parse(val title: String, val langlinks: List<Langlink>)
+data class Langlink(val lang: String, val url: String)
 
 @KtorExperimentalLocationsAPI
 fun Application.module() {
@@ -33,23 +36,19 @@ fun Application.module() {
     }
     routing {
         get<Index> { p ->
-            val start = System.currentTimeMillis();
-            log.info("start")
-
-            // async {...} 内が非同期処理
+            val uri = "https://ja.wikipedia.org/w/api.php?action=parse&format=json&page=${p.page}&prop=langlinks"
             val content = async {
                 HttpClient(Apache).use { client ->
-                    log.info("client request start: time ${System.currentTimeMillis() - start}ms")
-                    val content = client.call(p.url).response.readText()
-                    log.info("client request end: time ${System.currentTimeMillis() - start}ms")
-                    content
+                    client.call(uri).response.readText()
                 }
             }
-
-            log.info("client processed: time ${System.currentTimeMillis() - start}ms")
-
-            // await で非同期処理の完了を待ち、結果を取得できる
-            call.respondText(content.await(), ContentType.Text.Plain)
+            val parse = Gson().fromJson(content.await(), WikiPedia::class.java).parse ?: Parse("", emptyList())
+            val langlinks = when (p.lang.isEmpty()) {
+                true  -> parse.langlinks
+                false -> parse.langlinks.filter { it.lang == p.lang }
+            }
+            call.respond(parse.copy(langlinks = langlinks))
         }
     }
 }
+
