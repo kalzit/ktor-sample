ca8b0b27d47a88a59bf81da1d555617c418fc9b6
kalzit <1196868+kalzit@users.noreply.github.com>
2018-12-01 16:01:19 +0900

    use locations api.

--- src/Application.kt
+++ src/Application.kt
@@ -5,21 +5,30 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Location
+import io.ktor.locations.Locations
+import io.ktor.locations.get
 import io.ktor.response.respond
-import io.ktor.routing.get
 import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 data class Message(val content: String)
 
+@KtorExperimentalLocationsAPI
+@Location("/{name}")
+data class Index(val name: String)
+
+@KtorExperimentalLocationsAPI
 fun Application.module() {
+    install(Locations)
     install(ContentNegotiation) {
         gson()
     }
     routing {
-        get("/") {
-            call.respond(Message("Hello World!"))
+        get<Index>{ p ->
+            call.respond(Message("Hello, ${p.name}"))
         }
     }
 }
